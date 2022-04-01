a3f552050d026ccc6635f511757f59f7464e6318
6akcbaHu <>
2022-04-01 11:24:55 +0300

    update

--- test/routes/web.php
+++ test/routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
 //use App\Http\Middleware\AddHeaders;
+use App\Http\Controllers\FilmController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\{FilmController, UserController};
+
 
 /*
 |--------------------------------------------------------------------------
